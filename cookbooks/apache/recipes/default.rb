10686b0c2091c31ecc91b1a9245c35d96b164c05
Tyler Fitch <tfitch@getchef.com>
2014-10-30 10:56:28 -0600

    use of run_state

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -11,6 +11,21 @@ package "httpd" do
   action :install
 end
 
+ruby_block "randomly_choose_language" do
+  block do
+    if Random.rand > 0.5
+      node.run_state['scripting_language'] = 'php'
+    else
+      node.run_state['scripting_language'] = 'perl'
+    end
+  end
+end
+
+package "scripting_language" do
+  package_name lazy { node.run_state['scripting_language'] }
+  action :install
+end
+
 # Disable the default virtual host
 apache_vhost "welcome" do
   action :remove
